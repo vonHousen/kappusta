55c84979abc6d7aad4ebd9402ee4546a953711ae
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-06-04 17:29:15 +0200

    Added Category Spinner.

--- app/src/main/java/com/vonHousen/kappusta/ui/reporting/ReportingFragment.kt
+++ app/src/main/java/com/vonHousen/kappusta/ui/reporting/ReportingFragment.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.vonHousen.kappusta.MainActivity
 import com.vonHousen.kappusta.R
+import com.vonHousen.kappusta.paymentRecord.Category
 import com.vonHousen.kappusta.ui.history.HistoryViewModel
 import kotlinx.android.synthetic.main.fragment_reporting.*
+import java.time.LocalDate
 
 class ReportingFragment : Fragment() {
 
     private lateinit var reportingViewModel: ReportingViewModel
     private lateinit var historyViewModel: HistoryViewModel
+    private lateinit var selectedDate: LocalDate
+    private lateinit var selectedCategory: Category
+    private lateinit var paymentValueTxt: String
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -34,16 +41,44 @@ class ReportingFragment : Fragment() {
         button_reporting_ok.setOnClickListener {
             reportNow()
         }
+        configureCategorySpinner()
 
         // TODO listen to "OK" signal from keyboard
     }
 
     private fun reportNow() {
-        val reported =
-            reportingViewModel.processNewPaymentRecord(reporting_edit_text.text.toString())
+        paymentValueTxt = reporting_payment_edit_text.text.toString()
+        val reported = reportingViewModel.processNewPaymentRecord(paymentValueTxt)
         if(reported != null)
             historyViewModel.addPaymentToHistory(reported)
         (activity as MainActivity).showThingsAfterReporting()
     }
 
+    private fun configureCategorySpinner() {
+
+        val categoriesArray = resources.getStringArray(R.array.Categories)
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        val arrayAdapter = ArrayAdapter(
+            activity!!,
+            android.R.layout.simple_spinner_item,
+            categoriesArray
+        )
+        // Set layout to use when the list of choices appear
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        reporting_type_spinner!!.adapter = arrayAdapter
+        reporting_type_spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View, position: Int, id: Long
+            ) {
+                selectedCategory = reportingViewModel.categories[position]
+            }
+        }
+    }
 }
--- app/src/main/java/com/vonHousen/kappusta/ui/reporting/ReportingViewModel.kt
+++ app/src/main/java/com/vonHousen/kappusta/ui/reporting/ReportingViewModel.kt
@@ -1,9 +1,15 @@
 package com.vonHousen.kappusta.ui.reporting
 
 import androidx.lifecycle.ViewModel
+import com.vonHousen.kappusta.paymentRecord.Category
 import com.vonHousen.kappusta.paymentRecord.PaymentRecord
 
 class ReportingViewModel : ViewModel() {
+
+    val categories = arrayOf(
+        Category.DAILY,
+        Category.SPECIAL
+    )
 
     fun processNewPaymentRecord(reportingValue: String): PaymentRecord? {
         return if(reportingValue != "") {
