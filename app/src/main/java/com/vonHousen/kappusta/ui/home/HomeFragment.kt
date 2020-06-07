61848a0d03c7094d4257cda5edcc91718a6e627e
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-06-07 20:44:32 +0200

    First touch of home_fragment.

--- app/src/main/java/com/vonHousen/kappusta/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vonHousen/kappusta/ui/home/HomeFragment.kt
@@ -4,28 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.vonHousen.kappusta.R
+import com.vonHousen.kappusta.ui.history.HistoryViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var historyViewModel: HistoryViewModel
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        historyViewModel =
+            ViewModelProviders.of(activity!!).get(HistoryViewModel::class.java)
+
+
+        homeViewModel.textMoney.observe(viewLifecycleOwner, Observer {
+            text_home_money_left.text = it
+        })
+        homeViewModel.textPercentLeft.observe(viewLifecycleOwner, Observer {
+            text_home_percent_left.text = it
+        })
+        homeViewModel.textPercentAvg.observe(viewLifecycleOwner, Observer {
+            text_home_percent_avg.text = it
+        })
+    }
+
+
 }
--- app/src/main/java/com/vonHousen/kappusta/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/vonHousen/kappusta/ui/home/HomeViewModel.kt
@@ -6,8 +6,16 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _textMoney = MutableLiveData<String>().apply {
+        value = "700.77 zł"
     }
-    val text: LiveData<String> = _text
+    val textMoney: LiveData<String> = _textMoney
+    private val _textPercentLeft = MutableLiveData<String>().apply {
+        value = "77%"
+    }
+    val textPercentLeft: LiveData<String> = _textPercentLeft
+    private val _textPercentAvg = MutableLiveData<String>().apply {
+        value = "(-7%)"
+    }
+    val textPercentAvg: LiveData<String> = _textPercentAvg
 }
